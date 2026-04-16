7498686ecd4715dc2e9f615477c83124c157bcdf
Andrey Permyakov <ANDREYPE@rafael.co.il>
2026-04-16 11:31:04 +0300

    chore: initialize I2CBridge project structure with all references and packages

---
+++ tests/I2CBridge.Devices.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace I2CBridge.Devices.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/I2CBridge.Framework.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace I2CBridge.Framework.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
